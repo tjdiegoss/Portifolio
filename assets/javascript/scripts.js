c865dfe29220d08af6c5e3f2a22fd1036548ec8a
tjdiegoss <tjdiegoss@gmail.com>
2022-10-30 16:39:31 -0300

    - :up: Adicionando Scrool Button à Home

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,15 @@
+import initAnimationScroll from "animacao-scroll.js";
+import initMenuMobile from "menu-mobile.js";
+import initMenuTransition from "menu-transition.js";
+import initScrollSuave from "scroll-suave.js";
+import initTabNav from "tabnav.js";
+import initTyping from "typing.js";
+
+
+
+initTabNav();
+initScrollSuave();
+initAnimationScroll();
+initTyping();
+initMenuTransition();
+initMenuMobile();
